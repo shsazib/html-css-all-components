cbc8d8f3d3bb06c14f87d153b64eb61a46822cfe
shsazib <shsazibbhuiyan892939@gmail.com>
2022-11-20 03:42:00 -0800

    Payment Confirmation Cart add bottom button

---
+++ Buy Now Pop-up Cart/buyNowPopup.js
@@ -0,0 +1,18 @@
+const modalBox = document.querySelector(".modal-container");
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+
+
+modalBtn.addEventListener("click", () => {
+    modalBox.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+  modalBox.classList.remove('active');
+});
+modalBox.addEventListener('click', function(e) {
+    if (e.target.classList.contains('modal-container')) {
+        modalBox.classList.remove('active');   
+    }
+});
